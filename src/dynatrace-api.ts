ebfe5c1dad5087eb383c78c7229380a68add1c4d
Robin Wyss <me@robinwyss.com>
2024-04-30 08:49:57 +0200

    added additional logs

--- src/dynatrace-api.ts
+++ src/dynatrace-api.ts
@@ -2,6 +2,8 @@ import * as https from 'https';
 import { VulnerabilityType } from './types';
 import { LoggingService } from './LoggingService';
 
+const pageSize = 500;
+
 export class DynatraceApiClient {
     constructor(private tenantUrl: string, private token: string, private filterType: string, private filter: string, private logger: LoggingService) { }
 
@@ -9,9 +11,10 @@ export class DynatraceApiClient {
         this.logger.logInfo(`Fetching ${vulnerabilityType} vulnerabilities, filtered by ${this.filterType}: ${this.filter}`);
         let securityProblems: any[] = [];
         let securityProblemSelector = this.getSelector(vulnerabilityType);
-        let result = await this.callDynatraceAPI(`/api/v2/securityProblems?pageSize=100&securityProblemSelector=${securityProblemSelector}&fields=%2BcodeLevelVulnerabilityDetails%2C%2BriskAssessment&sort=-riskAssessment.riskScore&from=now-10m`);
+        let result = await this.callDynatraceAPI(`/api/v2/securityProblems?pageSize=${pageSize}&securityProblemSelector=${securityProblemSelector}&fields=%2BcodeLevelVulnerabilityDetails%2C%2BriskAssessment&sort=-riskAssessment.riskScore&from=now-10m`);
         securityProblems = securityProblems.concat(result.securityProblems);
         while (result.nextPageKey) {
+            this.logger.logInfo(`fetching next page ${result.nextPageKey}`);
             result = await this.callDynatraceAPI('/api/v2/securityProblems?nextPageKey=' + result.nextPageKey);
             securityProblems = securityProblems.concat(result.securityProblems);
         }
@@ -26,7 +29,7 @@ export class DynatraceApiClient {
     }
 
     private getSelector(vulnerabilityType: VulnerabilityType) {
-        let securityProblemSelector = 'vulnerabilityType(%22' + vulnerabilityType + '%22),status("OPEN")';
+        let securityProblemSelector = 'vulnerabilityType("' + vulnerabilityType + '"),status("OPEN")';
         if (this.filterType === "Workload") {
             securityProblemSelector += `,relatedKubernetesWorkloadNames("${this.filter}")`;
         } else if (this.filterType === "Management Zone") {
@@ -36,21 +39,23 @@ export class DynatraceApiClient {
         } else if (this.filterType === 'selector') {
             securityProblemSelector += ',' + this.filter;
         }
-        return securityProblemSelector;
+         return securityProblemSelector;
     }
 
     private async callDynatraceAPI(endpoint: string): Promise<any> {
         // const tenantUrl = getTenantUrl();
         const apiEndpoint = this.tenantUrl + endpoint;
         return new Promise((resolve, reject) => {
+            this.logger.logInfo(`Querying Dynatrace API: ${apiEndpoint}`);
             https.get(apiEndpoint, {
                 headers: {
                     "accept": "application/json; charset=utf-8",
                     "authorization": `Api-Token ${this.token}`
                 }
             }, res => {
+                this.logger.logInfo(`API response: ${res.statusCode}`);
                 if (res.statusCode && res.statusCode > 299) {
-                    this.logger.logError('Could not retrieve data from Dynatrace:' + res.statusMessage);
+                    this.logger.logError(`Could not retrieve data: ${res.statusCode} ${res.statusMessage}`);
                     reject(res.statusMessage);
                 }
                 let data: any[] = [];
@@ -58,6 +63,7 @@ export class DynatraceApiClient {
                     data.push(chunk);
                 });
                 res.on('error', error => {
+                    this.logger.logError(`Error while reading response ${error}`);
                     reject(error);
                 });
                 res.on('end', () => {
@@ -65,6 +71,7 @@ export class DynatraceApiClient {
                         const result = JSON.parse(Buffer.concat(data).toString());
                         resolve(result);
                     } catch {
+                        this.logger.logError(`Invalid response from ${endpoint}, verify Tenant URL `);
                         reject(`Invalid response from ${endpoint}, verify Tenant URL `);
                     }
                 });
